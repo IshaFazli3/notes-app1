f04810ba4200160d2b992ba6568a14ea845c1da6
Isha F <137927479+IshaFazli3@users.noreply.github.com>
2023-10-10 22:14:26 -0500

    Started JavaScript

---
+++ script.js
@@ -0,0 +1,40 @@
+const addBox = document.querySelector(".add-box"),
+popupBox = document.querySelector(".popup-box"),
+closeIcon = document.querySelector("header i");
+titleTag = popupBox.querySelector("input"),
+descTag = popupBox.querySelector("textarea"),
+addBtn = popupBox.querySelector("button");
+
+const months = ["January", "Febuary", "March", "April", "May",
+"June", "July", "August", "September", "October", "November", "December"];
+
+const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+addBox.addEventListener("click", () => {
+  popupBox.classList.add("show");
+});
+closeIcon.addEventListener("click", () => {
+  popupBox.classList.remove("show");
+} );
+
+addBtn.addEventListener("click", e => {
+  e.preventDefault();
+  let noteTitle = titleTag.value,
+  noteDesc = descTag.value;
+
+  if(noteTitle || noteDesc) {
+    let dateObj = new Date(), 
+    month = months[dateObj.getMonth()], 
+    day = dateObj.getDate(),
+    year = dateObj.getFullYear();
+
+    let noteInfo = {
+      title: noteTitle, description: noteDesc,
+      date: `${month} ${day}, ${year}`
+    }
+   
+   notes.push(noteInfo);
+   localStorage.setItem("notes", JSON.stringify(notes));
+   closeIcon.click();
+  }
+
+});
